904b37d653cf1ad18e6b11855e5ded39bac2f77f
Devang K <52554056+rising-entropy@users.noreply.github.com>
2022-05-01 09:14:36 +0530

    server

--- derma-cupid/config/server.js
+++ derma-cupid/config/server.js
@@ -1,7 +1,14 @@
-module.exports = ({ env }) => ({
+module.exports = ({ env }) => {
+  let theURL = "http://test.contriversedge.com"
+
+  return {
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
   },
-});
+  url: theURL,
+  proxy: false,
+  secure: false,
+  httpOnly: false,}
+};
