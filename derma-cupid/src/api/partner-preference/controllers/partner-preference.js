3abeca2851705792aa057872c5981f373f6a81c2
dushi1 <59909508+dushi1@users.noreply.github.com>
2022-05-06 13:42:02 +0530

    Models added.

---
+++ derma-cupid/src/api/partner-preference/controllers/partner-preference.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  partner-preference controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::partner-preference.partner-preference');
---
+++ derma-cupid/src/api/trust-score/controllers/trust-score.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  trust-score controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::trust-score.trust-score');
