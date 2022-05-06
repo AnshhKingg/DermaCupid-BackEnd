58e43684730a682992cbb3677af14c136f5d6ac5
Devang K <52554056+rising-entropy@users.noreply.github.com>
2022-05-06 23:30:24 +0530

    user profile

--- derma-cupid/src/api/theuser/controllers/theuser.js
+++ derma-cupid/src/api/theuser/controllers/theuser.js
@@ -9,17 +9,61 @@ const jwt_decode = require('jwt-decode');
 module.exports = {
   getProfile: async (ctx, next) => {
     try {
-      let token = ctx.request.headers['Authorization'];
-      let tokenData = jwt_decode(token);
-      ctx.body = tokenData;
-      return ctx;
+      let token = ctx.request.headers['authorization'];
+      try
+      {
+        let tokenData = jwt_decode(token)['data'];
+        let UID = tokenData['UID'];
+        let user = await strapi.db.query('api::app-user.app-user').findMany({
+          where: {UID},
+          populate: true
+        });
+        ctx.body = user[0]
+        return ctx;
+      }
+      catch(e)
+      {
+        ctx.body = {
+          message: 'Invalid Token'
+        }
+        ctx.status = 403
+        return ctx;
+      }
     } catch (err) {
       ctx.body = err;
     }
   },
   updateProfile: async (ctx, next) => {
     try {
-      ctx.body = 'ok';
+      let body = ctx.request.body;
+      let token = ctx.request.headers['authorization'];
+      try
+      {
+        let tokenData = jwt_decode(token)['data'];
+        let UID = tokenData['UID'];
+        let user = await strapi.db.query('api::app-user.app-user').updateMany({
+          where: {UID},
+          data: body
+        });
+        user = await strapi.db.query('api::app-user.app-user').findMany({
+          where: {UID},
+          populate: true
+        });
+        ctx.status = 201;
+        ctx.body = {
+          data: user[0],
+          message: "Updated Successfully!"
+        }
+        return ctx;
+      }
+      catch(e)
+      {
+        ctx.body = {
+          message: 'Invalid Token'
+        }
+        ctx.status = 403
+        return ctx;
+      }
     } catch (err) {
       ctx.body = err;
     }
